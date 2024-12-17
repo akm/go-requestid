62052d81b873fd0db0af6de83cdc397415e2c811
akm <akm2000@gmail.com>
2024-12-18 00:03:43 +0900

    ✨ Update function to be passed to slogw.Register

--- slog.go
+++ slog.go
@@ -9,12 +9,12 @@ import (
 
 func RegisterSlogHandle(key string) {
 	slogw.Register(
-		func(ctx context.Context, rec slog.Record) *slog.Record {
+		func(ctx context.Context, rec slog.Record) slog.Record {
 			requestID := Get(ctx)
 			if requestID != "" {
 				rec.Add(key, requestID)
 			}
-			return &rec
+			return rec
 		},
 	)
 }
