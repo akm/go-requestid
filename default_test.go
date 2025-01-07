40a4900bc07fce9d77559e04fa1c56647f2578e7
akm <akm2000@gmail.com>
2025-01-07 23:05:15 +0900

    ✅ Add test for Wrap function without options

--- default_test.go
+++ default_test.go
@@ -1,6 +1,9 @@
 package requestid
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +19,34 @@ func TestDefault(t *testing.T) {
 
 	assert.Equal(t, ns1, DefaultNamespace())
 	assert.Equal(t, ns1, DefaultNamespace(), "DefaultNamespace should return the same instance")
+}
+
+func TestDefaultSimpleWrap(t *testing.T) {
+	backupDefaultNamespace := degaultNamespace
+	defer func() { degaultNamespace = backupDefaultNamespace }()
+
+	baseHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, world!"))
+		assert.NoError(t, err)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ts := httptest.NewServer(
+		Wrap(baseHandler),
+	)
+	defer ts.Close()
+
+	t.Run("request", func(t *testing.T) {
+		req, err := http.NewRequest("GET", ts.URL, nil)
+		assert.NoError(t, err)
+
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello, world!", string(body))
+	})
 }
