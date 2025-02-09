bbd58e933fd76e8916e7fee660845eba908943a0
akm <akm2000@gmail.com>
2025-02-09 16:59:46 +0900

    ♻️ Simplify option functions by reducing multiline return statements

--- options.go
+++ options.go
@@ -23,33 +23,23 @@ func newDefaultOptions() *Options {
 type Option func(o *Options)
 
 func LogAttr(attr string) Option {
-	return func(o *Options) {
-		o.logAttr = attr
-	}
+	return func(o *Options) { o.logAttr = attr }
 }
 
 func Generator(g generator) Option {
-	return func(o *Options) {
-		o.generator = g
-	}
+	return func(o *Options) { o.generator = g }
 }
 
 // Set X-Cloud-Trace-Context for Google Cloud https://cloud.google.com/trace/docs/trace-context?hl=ja#http-requests
 // Set X-Amzn-Trace-Id for AWS https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/application/load-balancer-request-tracing.html
 func RequestHeader(h string) Option {
-	return func(o *Options) {
-		o.requestHeader = h
-	}
+	return func(o *Options) { o.requestHeader = h }
 }
 
 func ResponseHeader(h string) Option {
-	return func(o *Options) {
-		o.responseHeader = h
-	}
+	return func(o *Options) { o.responseHeader = h }
 }
 
 func SlogwNamespace(ns *slogctx.Namespace) Option {
-	return func(o *Options) {
-		o.slogctxNamespace = ns
-	}
+	return func(o *Options) { o.slogctxNamespace = ns }
 }
