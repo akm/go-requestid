dc5af84e27d169790a07cf1652e8c36a0b1963b6
akm <akm2000@gmail.com>
2025-02-10 00:40:45 +0900

    📝 Add doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package requestid provides a middleware that sets a unique request ID for each incoming HTTP request.
+And it also provides a logger that logs the request ID for each incoming HTTP request.
+*/
+package requestid
